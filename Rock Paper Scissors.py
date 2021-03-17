f425b0abd286e057ed78bb3aebd4da21e32e6577
Decheng Zheng <76794311+dz-pz@users.noreply.github.com>
2021-03-17 19:26:29 -0400

    Update Rock Paper Scissors.py
    Added features that prevent users from typing unrelated words.

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,14 +1,19 @@
 import random
 
 # Greeting
-play_or_not = input("Welcome to a game of Rock Paper Scissors. Would you like to play? (Yes/No): ").lower()
+play_or_no = input("Welcome to a game of Rock Paper Scissors, would you like to play? (Yes/No): ").lower()
 
 # Main
-while play_or_not == "yes":
-
+while play_or_no == "yes":
+https://github.com/
     random_number = random.randint(1, 3)
+
     # User's choice
     user_input = input("Rock, Paper, or Scissors?: ").lower()
+    choices = ["rock", "paper", "scissors"]
+    if user_input not in choices:
+        print("Please enter either rock paper or scissors")
+        continue
 
     # Computer's choice
     if random_number == 1:
@@ -24,15 +29,31 @@ while play_or_not == "yes":
     # Compare choices
     if user_input == "rock" and comp == "paper":
         print("You lose")
+        print("--------")
     elif user_input == "paper" and comp == "scissors":
         print("You lose")
+        print("--------")
     elif user_input == "scissors" and comp == "rock":
         print("You lose")
+        print("--------")
     elif user_input == comp:
         print("Tie")
+        print("---")
     else:
         print("You win")
-    play_or_not = input("Would you like to play again? (Yes/No): ").lower()
+        print("-------")
 
+    # Replay
+    while True:
+        play_again = input("Would you like to play again? (Yes/No): ").lower()
+        if play_again in ("yes", "no"):
+            break
+        print("Please enter yes or no")
+    if play_again == "yes":
+        continue
+
+    else:
+        print("Ok thank you for playing, have a good day!")
+        break
 else:
     print("Ok have a good day")
